46b4964bb68e848ebdf9654efddf736e93494e1b
dzmitry is <isayevich12@gmail.com>
2021-02-20 17:04:24 +0300

    add new project

---
+++ AdditionalTasks2_SortinganExpression/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdditionalTasks2_SortinganExpression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
