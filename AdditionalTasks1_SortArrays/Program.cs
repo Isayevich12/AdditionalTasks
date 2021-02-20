a6be60b5ad1b8b7a61d82a6d637b2b2ff068bb03
dzmitry is <isayevich12@gmail.com>
2021-02-20 19:02:10 +0300

    work with quicksort

--- AdditionalTasks1_SortArrays/Program.cs
+++ AdditionalTasks1_SortArrays/Program.cs
@@ -6,21 +6,29 @@ namespace AdditionalTasks1_SortArrays
     {
         static void Main(string[] args)
         {
-            MyArray myArr1 = new MyArray();
+            //MyArray myArr1 = new MyArray();
 
-            Console.WriteLine("Изначальный массив->");
-            myArr1.ShowArray();
-            Console.WriteLine(new string('_', 50));
+            //Console.WriteLine("Изначальный массив->");
+            //myArr1.ShowArray();
+            //Console.WriteLine(new string('_', 50));
 
-            Console.WriteLine("После сортировки->");
-            myArr1.SortArry(TypeSort.down);
+            //Console.WriteLine("После сортировки->");
+            //myArr1.SortArry(TypeSort.down);
 
-            myArr1.ShowArray();
+            //myArr1.ShowArray();
 
-            myArr1.SortArry(TypeSort.up);
+            //myArr1.SortArry(TypeSort.up);
 
-            myArr1.ShowArray();
+            //myArr1.ShowArray();
 
+
+            int[] my = { 10, 4, 2, 14, 67, 2, 11, 33, 1, 15, 87,68, 99 };
+            Sorter.QuickSort(my, 0, my.Length - 1);
+
+            foreach (var item in my)
+            {
+                Console.Write(item.ToString()+" ");
+            }
 
 
         }
