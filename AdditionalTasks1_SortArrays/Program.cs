0aa05a132e6d3df698684f2f83cbb2fa54584317
dzmitry is <isayevich12@gmail.com>
2021-02-20 14:26:32 +0300

    Добавьте файлы проекта.

---
+++ AdditionalTasks1_SortArrays/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdditionalTasks1_SortArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
