9468448348294aeaf2e9072661d59f4efa3b7168
dzmitry is <isayevich12@gmail.com>
2021-02-21 15:49:22 +0300

    добавил пустой проект

--- AdditionalTask3_UserCollectionQueueWithPriority/Program.cs
+++ AdditionalTask3_UserCollectionQueueWithPriority/Program.cs
@@ -6,7 +6,7 @@ namespace AdditionalTask3_UserCollectionQueueWithPriority
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!!!!!!");
+            Console.WriteLine("empty project!!!!!!");
         }
     }
 }
