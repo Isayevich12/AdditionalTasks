d505507ecb99bf777c9cd6a88040cb3d04ac2edd
dzmitry is <isayevich12@gmail.com>
2021-02-21 14:14:53 +0300

    d

---
+++ AdditionalTask3_UserCollectionQueueWithPriority/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdditionalTask3_UserCollectionQueueWithPriority
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!!!!!!");
+        }
+    }
+}
